b32c184c0cdfd6e7002d494b42e02c58e294d319
reisyun <reisyun7@gmail.com>
2021-06-10 09:40:51 +0900

    fix: Remove emotion label option

--- craco.config.js
+++ craco.config.js
@@ -3,9 +3,6 @@ const path = require('path');
 const resolve = dir => path.resolve(__dirname, dir);
 
 module.exports = {
-  babel: {
-    plugins: [['@emotion', { labelFormat: '[filename]--[local]' }]],
-  },
   webpack: {
     extensions: ['.js', '.ts', '.tsx', '.d.ts', '.json'],
     alias: {
