98d19bec35b9b394549076ce0699535e36a287f5
reisyun <reisyun7@gmail.com>
2021-06-11 18:44:27 +0900

    feat: Add toBase64 function

--- src/hooks/useWriteForm.ts
+++ src/hooks/useWriteForm.ts
@@ -1,11 +1,23 @@
-// import emailjs from 'emailjs-com';
+import { useEffect } from 'react';
+import emailjs from 'emailjs-com';
+import { config } from '@/libs/config';
 import { useThemeState, useSubjectState } from '@/atoms/questionState';
-import { useAnswerState } from '@/atoms/answerState';
+import { useAnswerState, useFileState } from '@/atoms/answerState';
+
+const toBase64 = (file: File) =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+  });
 
 export function useWriteForm() {
   const [theme] = useThemeState();
   const [subject] = useSubjectState();
   const [answerForm] = useAnswerState();
+  const [file] = useFileState();
+  const files = file.map(f => toBase64(f));
 
   const answer = answerForm
     .map(
@@ -24,16 +36,26 @@ export function useWriteForm() {
   };
 
   const sendEmail = async (form: any) => {
-    Email.send({
-      Host: 'smtp.elasticemail.com',
-      Username: 'reisyun7@gmail.com',
-      Password: '333DED114BDF5B58E94F6704AD0A3424A9C7',
-      To: 'reisyun7@gmail.com',
-      From: 'reisyun7@gmail.com',
-      Subject: 'This is the subject',
-      Body: 'And this is the body',
-    }).then(message => alert(message));
-    // await emailjs.send('question_tree', 'template_g9sreii', form);
+    const body = `
+<h3>${theme.name} | ${subject}</h3>
+<strong>연락처: ${form.phone}</strong><br/>
+<strong>이메일: ${form.email}</strong>
+<br />
+<br />
+<div>${answer}</div>
+`;
+
+    // await Email.send({
+    //   SecureToken: '7fa48da1-2a0e-4d81-9c57-ded46443058b',
+    //   To: 'reisyun7@gmail.com',
+    //   From: 'ab@gm.co',
+    //   Subject: `${theme.name} | ${form.phone}`,
+    //   Body: body,
+    //   Attachments: file.map(async f => ({
+    //     name: f.name,
+    //     path: 'https://networkprogramming.files.wordpress.com/2017/11/smtpjs.png',
+    //   })) as any,
+    // }).then(message => console.log(message));
   };
 
   return { template, sendEmail };
