0cb9f1c05031870864c2e9a9f9ad0d0e0f70b584
reisyun <reisyun7@gmail.com>
2021-06-11 18:40:04 +0900

    chore: Make pretty

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,7 @@
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/stable';
 import React from 'react';
-// import { config } from '@/libs/config';
 import ReactDOM from 'react-dom';
 import App from '@/components/App';
 
 ReactDOM.render(<App />, document.getElementById('my_react_app'));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
