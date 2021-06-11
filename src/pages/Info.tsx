6273301b7c8e982d0f004e6ecc11f343e55e128c
reisyun <reisyun7@gmail.com>
2021-06-11 18:41:47 +0900

    style: Modify info page layout

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { useHistory } from 'react-router-dom';
-import person from '@/assets/images/person.png';
 import Text from '@/components/common/Text';
 import { withStyles } from '@material-ui/core/styles';
 import { ButtonGroup, Button, Link, useMediaQuery } from '@material-ui/core';
@@ -9,9 +8,10 @@ import Icon from '@/components/common/Icon';
 
 function Info() {
   const history = useHistory();
-  const matches960 = useMediaQuery(`(max-width: 960px)`);
+  const matches680 = useMediaQuery(`(max-width: 680px)`);
+  const matches800 = useMediaQuery(`(max-width: 800px)`);
 
-  const iconSize = matches960 ? 'small' : 'medium';
+  const iconSize = matches800 ? 'small' : 'medium';
 
   const handleClick = () => {
     history.push('/choice');
@@ -21,67 +21,101 @@ function Info() {
     <InfoWrap>
       <Header>
         <Inner top>
-          <PersonImage src={person} />
+          <PersonImage
+            src="http://antiwhitepig.cafe24.com/wp-content/uploads/2021/06/person.png"
+            alt="person"
+          />
           <Point>
-            <Text fontSize="md" color="main" bold>
+            <Text fontSize={matches680 ? 'sm' : 'md'} color="main" bold>
               형사 전문 변호사에게 직접 물어보세요!
             </Text>
           </Point>
 
           <Titles>
-            <Text fontSize="title" color="white" bold>
+            <Title fontSize="title" color="white" bold>
               형사법률 리스크 진단
-            </Text>
-            <Text fontSize="md" color="white">
+            </Title>
+            <Text fontSize={matches680 ? 'md' : 'lg'} color="white">
               법률과 관련된 고민을 쉽게 상담받아보세요
             </Text>
           </Titles>
-          <StyledButton
-            variant="contained"
-            size="large"
-            endIcon={<Icon icon="arrow" color="main" />}
-            onClick={handleClick}
-          >
-            시작하기
-          </StyledButton>
+          {matches680 ? (
+            <StyledMobileButton
+              variant="contained"
+              size="large"
+              endIcon={<Icon icon="arrow" color="main" />}
+              onClick={handleClick}
+            >
+              시작하기
+            </StyledMobileButton>
+          ) : (
+            <StyledPCButton
+              variant="contained"
+              size="large"
+              endIcon={<Icon icon="arrow" color="main" />}
+              onClick={handleClick}
+            >
+              시작하기
+            </StyledPCButton>
+          )}
         </Inner>
       </Header>
       <Bottom>
         <Inner>
           <Infos>
             <NameGroup>
-              <Text fontSize={matches960 ? 'md' : 'lg'} color="main">
+              <Text fontSize={matches800 ? 'md' : 'lg'} color="main">
                 형사전문변호사
               </Text>
-              <Text fontSize={matches960 ? 'xl' : 'subtitle'} color="main" bold>
+              <Text fontSize={matches800 ? 'xl' : 'subtitle'} color="main" bold>
                 김기윤
               </Text>
             </NameGroup>
             <MuiButtonGroup
               variant="text"
-              orientation={matches960 ? 'vertical' : 'horizontal'}
+              orientation={matches800 ? 'vertical' : 'horizontal'}
               size="large"
               color="primary"
             >
-              <LinkButton startIcon={<Icon icon="call" size={iconSize} color="main" />}>
-                <Link href="tel:025222218" target="_blank" rel="noopener">
+              <Button
+                style={matches800 ? MobileButtonStyle : PcButtonStyle}
+                startIcon={<Icon icon="call" size={iconSize} color="main" />}
+              >
+                <Link
+                  style={{ color: '#273C75' }}
+                  href="tel:025222218"
+                  target="_blank"
+                  rel="noopener"
+                >
                   02-522-2218
                 </Link>
-              </LinkButton>
-              <LinkButton startIcon={<Icon icon="chat" size={iconSize} color="main" />}>
-                <Link href="https://pf.kakao.com/_Dkaxixd/chat" target="_blank" rel="noopener">
+              </Button>
+              <Button
+                style={matches800 ? MobileButtonStyle : PcButtonStyle}
+                startIcon={<Icon icon="chat" size={iconSize} color="main" />}
+              >
+                <Link
+                  style={{ color: '#273C75' }}
+                  href="https://pf.kakao.com/_Dkaxixd/chat"
+                  target="_blank"
+                  rel="noopener"
+                >
                   카톡상담
                 </Link>
-              </LinkButton>
-              <LinkButton startIcon={<Icon icon="info" size={iconSize} color="main" />}>
+              </Button>
+              <Button
+                style={matches800 ? MobileButtonStyle : PcButtonStyle}
+                startIcon={<Icon icon="info" size={iconSize} color="main" />}
+              >
                 <Link
+                  style={{ color: '#273C75' }}
                   href="https://blog.naver.com/hopelawpasan/222377220296"
                   target="_blank"
                   rel="noopener"
                 >
                   변호사 소개
                 </Link>
-              </LinkButton>
+              </Button>
             </MuiButtonGroup>
           </Infos>
         </Inner>
@@ -107,18 +141,29 @@ const PersonImage = styled.img`
 
   position: absolute;
   right: 0;
-  bottom: 0;
+  /* bottom: -56vh; */
 
-  height: 90vh;
+  height: 84vh;
   min-height: 600px;
-  max-height: 1000px;
+  max-height: 900px;
 
   @media (max-width: 800px) {
-    max-height: 60vh;
+    max-height: 70vh;
+    bottom: -63vh;
   }
 
   @media (min-width: 1200px) {
-    max-height: 100vw;
+    max-height: 100vh;
+  }
+
+  @media (min-width: 900px) and (min-height: 1200px) {
+    max-height: 1000px;
+    bottom: -63vh;
+  }
+
+  @media (max-width: 800px) and (max-height: 900px) {
+    width: 33vh;
+    bottom: -63vh;
   }
 `;
 
@@ -127,19 +172,27 @@ const Header = styled.header`
   height: 60vh;
   min-height: 400px;
   background: ${({ theme }) => theme.palette.color.main};
+
+  @media (max-width: 680px) {
+    height: 53vh;
+  }
 `;
 
 const Inner = styled.div`
   position: absolute;
-  top: ${({ top }: { top?: boolean }) => (top ? 60 : 50)}%;
+  top: ${({ top }: { top?: boolean }) => (top ? 65 : 38)}%;
   left: 50%;
   padding: 0 40px;
   width: 100%;
-  max-width: 900px;
   transform: translate(-50%, -50%);
 
-  @media (min-width: 1200px) {
-    max-width: 1080px;
+  @media (max-width: 680px) {
+    top: ${({ top }: { top?: boolean }) => (top ? 56 : 30)}%;
+    padding: 0 24px;
+  }
+
+  @media (min-width: 800px) {
+    max-width: 960px;
   }
 `;
 
@@ -148,6 +201,10 @@ const Point = styled.div`
   padding: 8px 32px;
   background: rgba(255, 255, 255, 0.6);
   border-radius: 24px;
+
+  @media (max-width: 680px) {
+    padding: 8px 20px;
+  }
 `;
 
 const Titles = styled.div`
@@ -156,7 +213,30 @@ const Titles = styled.div`
   margin-top: 4px;
 `;
 
-const StyledButton = withStyles({
+const Title = styled(Text)`
+  font-size: 48px;
+  letter-spacing: -2px;
+
+  @media (max-width: 680px) {
+    font-size: 36px;
+  }
+`;
+
+const StyledPCButton = withStyles({
+  root: {
+    zIndex: 100,
+    marginTop: 40,
+    background: 'white',
+    width: 160,
+    height: 48,
+  },
+  label: {
+    color: '#273C75',
+    fontWeight: 'bold',
+  },
+})(Button);
+
+const StyledMobileButton = withStyles({
   root: {
     zIndex: 100,
     marginTop: 40,
@@ -172,12 +252,16 @@ const Bottom = styled.footer`
   position: relative;
   height: 40vh;
   min-height: 200px;
+
+  @media (max-width: 680px) {
+    height: 47vh;
+  }
 `;
 
 const Infos = styled.div`
   display: inline-flex;
   flex-direction: column;
-  padding: 24px 16px;
+  padding: 12px;
 
   max-width: 1200px;
   min-height: 120px;
@@ -188,16 +272,21 @@ const Infos = styled.div`
 
 const NameGroup = styled.div`
   display: inline-flex;
-  margin-left: 16px;
-  margin-bottom: 16px;
+  align-items: flex-end;
+  margin-bottom: 12px;
 
   & span {
-    margin-right: 8px;
+    margin-left: 12px;
     line-height: 1.2;
   }
 
-  @media (max-width: 960px) {
+  @media (max-width: 800px) {
     flex-direction: column;
+    align-items: flex-start;
+
+    & span {
+      margin-left: 0;
+    }
   }
 `;
 
@@ -205,12 +294,18 @@ const MuiButtonGroup = withStyles({
   root: {
     position: 'relative',
   },
+  grouped: {
+    fontSize: '0.875em',
+  },
 })(ButtonGroup);
 
-const LinkButton = withStyles({
-  root: {
-    padding: '8px 16px',
-  },
-})(Button);
+const PcButtonStyle = {
+  padding: '8px 16px',
+};
+
+const MobileButtonStyle = {
+  padding: '8px 0',
+  justifyContent: 'flex-start',
+};
 
 export default Info;
