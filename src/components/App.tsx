c30a7f223234a95f906f27b1f9034e77eb40166f
reisyun <reisyun7@gmail.com>
2021-06-11 18:40:26 +0900

    feat: Add material ui theme

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,6 +6,19 @@ import theme from '@/styles/theme';
 import global from '@/styles/global';
 import { routes } from '@/libs/routes';
 
+import { createMuiTheme, ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+
+const muiTheme = createMuiTheme({
+  typography: {
+    fontFamily: 'Noto Sans KR regular',
+  },
+  palette: {
+    primary: {
+      main: '#273C75',
+    },
+  },
+});
+
 function App() {
   const route = routes.map(({ path, component, exact }) => (
     <Route key={path} path={path} component={component} exact={exact} />
@@ -13,12 +26,14 @@ function App() {
 
   return (
     <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <Global styles={global} />
-        <Router>
-          <Switch>{route}</Switch>
-        </Router>
-      </ThemeProvider>
+      <MuiThemeProvider theme={muiTheme}>
+        <ThemeProvider theme={theme}>
+          <Global styles={global} />
+          <Router>
+            <Switch>{route}</Switch>
+          </Router>
+        </ThemeProvider>
+      </MuiThemeProvider>
     </RecoilRoot>
   );
 }
