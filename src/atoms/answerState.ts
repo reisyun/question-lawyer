1c52cdb4684e0f2fbfdca763826f33fba06447f6
reisyun <reisyun7@gmail.com>
2021-06-11 18:29:13 +0900

    fix: Change file hooks name

--- src/atoms/answerState.ts
+++ src/atoms/answerState.ts
@@ -1,12 +1,22 @@
 import { atom, useRecoilState } from 'recoil';
 
 export type AnswerType = { q: string; a: string };
+export type FileType = File;
 
 export const answerState = atom<AnswerType[]>({
   key: 'answerState',
   default: [],
 });
 
+export const fileState = atom<FileType[]>({
+  key: 'fileState',
+  default: [],
+});
+
 export function useAnswerState() {
   return useRecoilState(answerState);
+}
+
+export function useFileState() {
+  return useRecoilState(fileState);
 }
