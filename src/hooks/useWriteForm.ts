d2a6503715aa77c1008e7fd475193d36dba33922
reisyun <reisyun7@gmail.com>
2021-06-28 09:20:14 +0900

    fix: Use emailjs

--- src/hooks/useWriteForm.ts
+++ src/hooks/useWriteForm.ts
@@ -1,55 +1,89 @@
-import axios from 'axios';
+// import axios from 'axios';
+import { send } from 'emailjs-com';
+import { config } from '@/libs/config';
 import { useThemeState, useSubjectState } from '@/atoms/questionState';
-import { useAnswerState, useFileState } from '@/atoms/answerState';
+import { useAnswerState, useFileState, AnswerType } from '@/atoms/answerState';
 
-axios.defaults.baseURL = 'https://lawyer-question-tree.herokuapp.com';
-// axios.defaults.baseURL = 'http://localhost:4000';
+interface FormData {
+  theme: string;
+  subject: string;
+  answers: AnswerType[];
+  phone: string;
+  email: string;
+  files: File[];
+}
+const createMailBody = (form: FormData) => {
+  const { theme, subject, answers, phone, email, files } = form;
 
-const writeFormData = (form: any) => {
-  const { phone, email, theme, subject, answer } = form;
-  const files = form.files as File[];
+  const answerHTML = answers
+    .map(({ q, a }) => {
+      const isAnswers = a.split(/&/g);
+      return `질문: ${q}<br />답변: ${
+        isAnswers.length === 1 ? a : `<li>${isAnswers.join('</li><li>')}</li>`
+      }`;
+    })
+    .join('<br /><br />');
 
-  const fd = new FormData();
-  fd.append('phone', phone);
-  fd.append('email', email);
-  fd.append('theme', theme);
-  fd.append('subject', subject);
-  fd.append('answer', answer);
-  // 리스트 형식으로 formData 담을 수 없음
-  files.map((file: File) => fd.append(`files`, file));
+  const mailBody = `
+  ${theme !== subject ? `<h3>${theme} | ${subject}</h3>` : `<h3>${theme}</h3>`}
+  <strong>연락처: ${phone}</strong><br/>
+  ${email && `<strong>이메일: ${email}</strong>`}
+  <br />
+  <br />
+  <div>${answerHTML}</div>
+  `;
 
-  return fd;
-};
-
-const postFormDataToServer = async (data: FormData) => {
-  await axios.post('/send', data);
+  return mailBody;
 };
 
 export function useWriteForm() {
   const [theme] = useThemeState();
   const [subject] = useSubjectState();
-  const [answerForm] = useAnswerState();
+  const [answers] = useAnswerState();
   const [files] = useFileState();
-
-  const answerHTML = answerForm
-    .map(({ q, a }) => `질문: ${q}<br />답변: ${a || 'skip'}`)
-    .join('<br /><br />');
 
   const sendEmail = async (form: any) => {
     const { phone, email } = form;
 
-    const formData = writeFormData({
+    const body = createMailBody({
       theme: theme?.name,
       subject,
-      answer: answerHTML,
+      answers,
       phone,
       email,
       files,
     });
 
-    // eslint-disable-next-line no-console
-    await postFormDataToServer(formData).then(console.log).catch(console.error);
+    await send(config.EMAILJS_SERVICE_ID, config.EMAILJS_TEMPLATE_ID, {
+      phone,
+      body,
+    });
   };
 
   return sendEmail;
 }
+
+// axios.defaults.baseURL = 'https://lawyer-question-tree.herokuapp.com';
+// axios.defaults.baseURL = 'http://localhost:4000';
+
+// const writeFormData = (form: any) => {
+//   const { phone, email, theme, subject, answer } = form;
+//   const files = form.files as File[];
+
+//   const fd = new FormData();
+//   fd.append('phone', phone);
+//   fd.append('email', email);
+//   fd.append('theme', theme);
+//   fd.append('subject', subject);
+//   fd.append('answer', answer);
+//   // 리스트 형식으로 formData 담을 수 없음
+//   files.map((file: File) => fd.append(`files`, file));
+
+//   return fd;
+// };
+
+// const postFormDataToServer = async (data: FormData) => {
+//   await axios.post('/send', data);
+// };
+
+// await postFormDataToServer(formData).then(console.log).catch(console.error);
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { init } from 'emailjs-com';
+import { config } from '@/libs/config';
 import App from '@/components/App';
+
+init(config.EMAILJS_USER_ID);
 
 ReactDOM.render(<App />, document.getElementById('my_react_app'));
