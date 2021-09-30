8abe13cc397c6856e642f72bd86bfa1525f47f32
reisyun <reisyun7@gmail.com>
2021-09-30 15:54:14 +0900

    chore: Add example email confg

---
+++ src/libs/config.example.ts
@@ -0,0 +1,10 @@
+/**
+ * 이곳에서 ID를 확인 할 수 있습니다.
+ * https://dashboard.emailjs.com/admin
+ */
+
+export const config = {
+  EMAILJS_USER_ID: 'user_id',
+  EMAILJS_SERVICE_ID: 'service_id',
+  EMAILJS_TEMPLATE_ID: 'template_id',
+};
